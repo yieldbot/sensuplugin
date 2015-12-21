00e9c206864438d8e985183d9a000ad93f642f57
Matt Jones <mattjones@yieldbot.com>
2015-12-20 22:57:41 -0500

    refactor handler package

--- handler/common.go
+++ handler/common.go
@@ -5,9 +5,3 @@
 //   Copyright 2015 Yieldbot. <devops@yieldbot.com>
 //   Released under the MIT License; see LICENSE
 //   for details.
-
-package handler
-
-// Debug  Do we print debug statements or not? This is set in each binary but is placed here
-// to avoid the use of global variables
-var Debug bool
