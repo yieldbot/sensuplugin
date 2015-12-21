3f340fb0a264fd318d5973c789d52cdad76a0631
Matt Jones <mattjones@yieldbot.com>
2015-12-20 22:57:50 -0500

    add initial check package

---
+++ check/functions.go
@@ -0,0 +1,22 @@
+// Library for check related functions used by the Yieldbot Infrastructure
+// teams in sensu.
+//
+// LICENSE:
+//   Copyright 2015 Yieldbot. <devops@yieldbot.com>
+//   Released under the MIT License; see LICENSE
+//   for details.
+
+package check
+
+import (
+	"fmt"
+	"os"
+)
+
+// EHndlr is for generic error handling in all Yieldbot monitoring packages.
+func EHndlr(e error) {
+	if e != nil {
+		fmt.Printf("ERROR: %v", e)
+		os.Exit(129)
+	}
+}
