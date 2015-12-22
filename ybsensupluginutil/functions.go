c354660aabba15610eaecc1dde975eb27ece4a64
Matt Jones <mattjones@yieldbot.com>
2015-12-22 11:17:04 -0500

    add universal exit method

--- ybsensupluginutil/functions.go
+++ ybsensupluginutil/functions.go
@@ -11,6 +11,7 @@ package ybsensupluginutil
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 // EHndlr is for generic error handling in all Yieldbot monitoring packages.
@@ -18,5 +19,20 @@ func EHndlr(e error) {
 	if e != nil {
 		fmt.Printf("ERROR: %v", e)
 		os.Exit(129)
+	}
+}
+
+// Exit method for all sensu checks that will print the output and desired
+// exit code
+func Exit(s string, o string) {
+	// YELLOW need to make sure that condition exists
+	var exitCode int
+
+	for k := range ybsensupluginutil.MonitoringErrorCodes {
+		if k == strings.ToUpper(s) {
+			exitCode = ybsensupluginutil.MonitoringErrorCodes[k]
+		}
+		fmt.Printf("%v", o)
+		os.Exit(exitCode)
 	}
 }
