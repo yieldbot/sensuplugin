52b10acbb9a2e36fb035a0c6e429f34140e44563
Matt Jones <mattyjones@users.noreply.github.com>
2016-02-25 13:29:16 -0500

    Update common.go

--- sensuutil/common.go
+++ sensuutil/common.go
@@ -17,7 +17,7 @@ var MonitoringErrorCodes = map[string]int{
 	"PERMISSIONERROR":    126, // not executable, etc
 	"RUNTIMEERROR":       42,  // self explantory
         "DEBUG":              37,  // exit w/ debugging output
-	"OK":                 0,   // “I'd far rather be happy than right any day.”
+	"OK":                 0,   // “We’re still flying”
 	"WARNING":            1,   // this kinda sucks but don't get out of bed to deal with it
 	"CRITICAL":           2,   // “The ships hung in the sky in much the same way that bricks don't.” 
 	"UNKNOWN":            3,   // Would it save you a lot of time if I just gave up and went mad now?
