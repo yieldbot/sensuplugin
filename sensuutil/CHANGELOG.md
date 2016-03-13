5583e231aab36b81d3288e62bb93a7c0b850602f
Matt Jones <mattjones@yieldbot.com>
2016-03-12 21:35:42 -0500

    initial cleanup

--- sensuutil/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-#Change Log
-This CHANGELOG follows the format listed at [Keep A Changelog](http://keepachangelog.com/)
-
-## [Unreleased][unreleased]
-
--- sensuutil/common.go
+++ sensuutil/common.go
@@ -16,9 +16,9 @@ var MonitoringErrorCodes = map[string]int{
 	"CONFIGERROR":        127, // unix config error, not enough parms, etc
 	"PERMISSIONERROR":    126, // not executable, etc
 	"RUNTIMEERROR":       42,  // self explantory
-  "DEBUG":              37,  // exit w/ debugging output
+	"DEBUG":              37,  // exit w/ debugging output
 	"OK":                 0,   // “I'd far rather be happy than right any day.”
 	"WARNING":            1,   // this kinda sucks but don't get out of bed to deal with it
-	"CRITICAL":           2,   // “The ships hung in the sky in much the same way that bricks don't.” 
+	"CRITICAL":           2,   // “The ships hung in the sky in much the same way that bricks don't.”
 	"UNKNOWN":            3,   // Would it save you a lot of time if I just gave up and went mad now?
 }
