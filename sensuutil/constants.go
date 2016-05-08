2b5cc63ab9b4a4249d7febaf8069ee2358fe2507
Matt Jones <mattjones@yieldbot.com>
2016-05-07 21:51:30 -0400

    add stderr formater

--- sensuutil/constants.go
+++ sensuutil/constants.go
@@ -16,3 +16,7 @@ type Password string
 var SyslogFormat = logging.MustStringFormatter(
      `%{time:15:04:05.000} ▶ %{level} %{message}`,
 )
+var StderrFormat = logging.MustStringFormatter(
+  `%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+
+)
