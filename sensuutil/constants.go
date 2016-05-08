8a34e224f4957afe9edb99a9bfbc5edf673446e0
Matt Jones <mattjones@yieldbot.com>
2016-05-07 21:54:28 -0400

    modify stderr formater

--- sensuutil/constants.go
+++ sensuutil/constants.go
@@ -17,6 +17,6 @@ var SyslogFormat = logging.MustStringFormatter(
      `%{time:15:04:05.000} ▶ %{level} %{message}`,
 )
 var StderrFormat = logging.MustStringFormatter(
-  `%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+  `%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level} %{id:03x}%{color:reset} %{message}`,
 
 )
